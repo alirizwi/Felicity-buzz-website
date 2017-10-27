10e87d25d611feade3ba01cc5cf63802ad68a7bb
yudhik11 <yudhik100@gmail.com>
2017-10-27 21:52:29 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Felicity Buzz Website
+
 We are currently using [webslides](https://webslides.tv/) to quickly prototype a visually appealing placeholder website in this branch
 
 Rough documentation can be found [here](https://webslides.tv/demos/components#slide=2)
--- yourApp.js
+++ yourApp.js
@@ -1,10 +1,11 @@
 var planet = planetaryjs.planet();
-// You can remove this statement if `world-110m.json`
-// is in the same path as the HTML page:
+/* You can remove this statement if `world-110m.json`
+ is in the same path as the HTML page:
+*/
 planet.loadPlugin(planetaryjs.plugins.earth({
   topojson: { file: 'world-110m.json' }
 }));
-// Make the planet fit well in its canvas
+/* Make the planet fit well in its canvas */
 planet.projection.scale(250).translate([1000, 1000]);
 var canvas = document.getElementById('globe');
 planet.draw(canvas);
